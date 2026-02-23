13f665e6ed988742028f643225e576c570388ce7
Ken Lawson <samuelgordonlawson@gmail.com>
2026-02-22 23:26:23 -0600

    House joker

---
+++ localization/en-us/joker.lua
@@ -0,0 +1,12 @@
+return {
+    descriptions = {
+        Joker = {
+            j_cb_house = {
+                name = "Mr. House",
+                text = {
+                    "{C:inactive,s:0.8}No effect yet"
+                }
+            }
+        }
+    }
+}
